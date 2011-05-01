e081af204581b6cd7491fa4193513c7235d6cd57
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-05-01 21:54:56 +0900

    Person#gaeのspecを作成

--- 01/spec/person_spec.rb
+++ 01/spec/person_spec.rb
@@ -9,4 +9,11 @@ describe "Person" do
     shimao.gender.should == :male
     shimao.birthday.should == Date.new(2007, 7, 31)
   end
+  it "Person#age で年齢を取得できるべき" do
+    shimao = Person.new(name: "shimao", gender: :male, birthday: Date.new(2007, 7, 31))
+    Date.stub!(:today) { Date.new(2011, 7, 30)}
+    shimao.age.should == 3
+    Date.stub!(:today) { Date.new(2011, 7, 31)}
+    shimao.age.should == 4
+  end
 end
