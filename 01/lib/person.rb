5f5a9cdb8f7f8f675520f4349d1d57608cbce5ba
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-05-04 03:16:40 +0900

    Person#sayの実装を行いました。あえて冗長な記述をしています。コメントにリファクタリング後の例を書いています(ユーザーにはコメント部分は見せません)

--- 01/lib/person.rb
+++ 01/lib/person.rb
@@ -20,4 +20,37 @@ class Person
     # http://itpro.nikkeibp.co.jp/article/Watcher/20070822/280097/
     (Date.today.strftime("%Y%m%d").to_i - @birthday.strftime("%Y%m%d").to_i) / 10000
   end
+
+  def say
+    # リファクタリング前なので冗長な記述
+    now = Time.now
+    result = nil
+    if 4 <= now.hour and now.hour < 12
+      result = "Good morning!"
+    end
+    if 12 <= now.hour and now.hour < 18
+      result = "Good afternoon!"
+    end
+    if 18 <= now.hour and now.hour < 22
+      result = "Good evening!"
+    end
+    if (22 <= now.hour and now.hour < 24) or
+        (0 <= now.hour and now.hour < 4)
+      result = "Good night!"
+    end
+    result
+
+    # リファクタリング例としてはこんなカンジ
+#     now = Time.now
+#     case now.hour
+#     when 4..11
+#       "Good morning!"
+#     when 12..17
+#       "Good afternoon!"
+#     when 18..21
+#       "Good evening!"
+#     else
+#       "Good night!"
+#     end
+  end
 end
